6ab0f74130591ab96db3ef0e2123d21af2df9ce3
MeowChau <ChauNM.B23CC020@stu.ptit.edu.vn>
2025-04-06 21:02:49 +0700

    Cập nhật app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Layout, ConfigProvider } from 'antd';
-import Header from './components/Header';
-import HomePage from './pages/HomePage';
-import JobPostPage from './pages/JobPostPage';
-import JobDetailPage from './pages/JobDetailPage';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home/Home';
+import JobDetails from './components/Job/JobDetails';
+import SearchResults from './components/SearchResults';
+import CompanyList from './components/Company/CompanyList';
+import CompanyDetails from './components/Company/CompanyDetails';
+import EmployerSignup from './components/Auth/EmployerSignup';
 
-const { Content } = Layout;
-
-function App() {
-  return (
-    <ConfigProvider theme={{ token: { colorPrimary: '#1890ff' } }}>
-      <Router>
-        <Layout style={{ minHeight: '100vh' }}>
-          <Header />
-          <Content style={{ padding: '24px 50px', marginTop: 64 }}>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/post-job" element={<JobPostPage />} />
-              <Route path="/jobs/:id" element={<JobDetailPage />} />
-            </Routes>
-          </Content>
-        </Layout>
-      </Router>
-    </ConfigProvider>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Navbar />
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/job/:id" component={JobDetails} />
+                <Route path="/search" component={SearchResults} />
+                <Route path="/companies" component={CompanyList} />
+                <Route path="/company/:id" component={CompanyDetails} />
+                <Route path="/signup-employer" component={EmployerSignup} />
+            </Switch>
+        </Router>
+    );
+};
 
 export default App;
